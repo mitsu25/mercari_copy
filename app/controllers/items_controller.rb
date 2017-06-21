e58da5a4d16cf220be8bb4224aefdf6e12fa4ae6
mitsu25 <m.nakano65@gmail.com>
2017-06-21 21:57:54 +0900

    add items buy action as temporary action for making view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,7 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def buy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root                          "items#index"
   devise_for :users
-  resources  :items,             only: [:index, :show]
+  resources  :items,             only: [:index, :show] do
+    collection do
+      get 'buy'
+    end
+  end
   resources  :addresses,         only: [:new, :create]
   resources  :creditcards,       only: [:new, :create]
 end
