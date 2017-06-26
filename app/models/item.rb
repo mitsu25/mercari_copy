d269dcfdf7cef6e977f99e70b82bc73539dfb424
mitsu25 <m.nakano65@gmail.com>
2017-06-26 13:41:11 +0900

    revise a typo in an association

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   belongs_to :brand
   belongs_to :category
   belongs_to :subcategory
-  belongs_to :sller
+  belongs_to :seller
   belongs_to :buyer
   has_many   :imagess
 end
