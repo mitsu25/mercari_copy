886f183eb07bf8abec2415e820913b1c2ad05e8a
mitsu25 <m.nakano65@gmail.com>
2017-06-28 16:56:08 +0900

    small revise in schema file

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170628043557) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.index ["root_category_id"], name: "index_categories_on_root_category_id", using: :btree
+  end
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "item_id"
