e2d1ff7e8f948a285c383e9b553f07815973b973
mitsu25 <m.nakano65@gmail.com>
2017-07-01 16:18:04 +0900

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 before_exec do |server|
   ENV['BUNDLE_GEMFILE'] = File.expand_path('Gemfile', "#{app_path}/current")
 end
