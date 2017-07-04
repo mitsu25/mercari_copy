2ea6d968f8cb10d95e2dced1d3d3750dde112054
mitsu25 <m.nakano65@gmail.com>
2017-07-04 17:41:39 +0900

    add category sub_category data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@ root_categories_csv.shift
 root_categories_csv.each do |row|
   RootCategory.create(name: row[1], created_at: row[2], updated_at: row[3])
 end
+
+categories_csv = CSV.readlines("db/categories.csv")
+categories_csv.shift
+categories_csv.each do |row|
+  Category.create(name: row[1], root_category_id: row[2], created_at: row[3], updated_at: row[4])
+end
+
+sub_categories_csv = CSV.readlines("db/sub_categories.csv")
+sub_categories_csv.shift
+sub_categories_csv.each do |row|
+  SubCategory.create(name: row[1], category_id: row[2], created_at: row[3], updated_at: row[4])
+end
