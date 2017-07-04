6b24f31ad6994288363aec47ba091cece3c67f29
mitsu25 <m.nakano65@gmail.com>
2017-07-04 17:13:37 +0900

    fix seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-require “csv”
+require "csv"
 
-root_categories_csv = CSV.readlines(“db/root_categories.csv”)
+root_categories_csv = CSV.readlines("db/root_categories.csv")
 root_categories_csv.shift
 root_categories_csv.each do |row|
   RootCategory.create(name: row[1], created_at: row[2], updated_at: row[3])
