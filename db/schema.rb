e960a72f575a60448b5b2e65366451c389b7834d
mitsu25 <m.nakano65@gmail.com>
2017-07-04 18:11:19 +0900

    create followers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629065954) do
+ActiveRecord::Schema.define(version: 20170704090016) do
 
   create_table "buyers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",    null: false
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20170629065954) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.index ["root_category_id"], name: "index_categories_on_root_category_id", using: :btree
+  end
+
+  create_table "followers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",          null: false
+    t.integer  "followed_user_id", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["followed_user_id"], name: "index_followers_on_followed_user_id", using: :btree
+    t.index ["user_id"], name: "index_followers_on_user_id", using: :btree
   end
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
