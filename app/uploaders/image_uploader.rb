e860656bea1b86ad96b72ec964f51f3f16c42632
mitsu25 <m.nakano65@gmail.com>
2017-07-06 22:07:35 +0900

    set image fix carrier image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,7 +18,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # サムネイルを生成する設定
   version :thumb do
-    process :resize_to_limit => [75, 755]
+    process :resize_to_limit => [75, 75]
   end
 
   # jpg,jpeg,gif,pngしか受け付けない
