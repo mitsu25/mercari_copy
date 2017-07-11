dc00a5dda3679f867b6559dc314b66bfe4305e49
mitsu25 <m.nakano65@gmail.com>
2017-07-11 18:36:33 +0900

    fix users & items controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!, only: :sell_list
+
   def show
-    @user = User.find(user_params[:id])
+    get_user_items(user_params[:id])
+    @follower = Follower.where(user_id: current_user.id).find_by(followed_user_id: user_params[:id]) if user_signed_in?
+  end
+
+  def sell_list
+    get_user_items(current_user.id)
     @items = @user.items.each_slice(3).to_a
-    @follower = Follower.where(user_id: current_user.id).find_by(followed_user_id: user_params[:id])
   end
 
 private
   def user_params
     params.permit(:id)
+  end
+
+  def get_user_items(id)
+    @user     = User.find(id)
+    @items    = @user.items.each_slice(3).to_a
   end
 end
