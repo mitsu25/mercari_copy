a2b7794927c409ce65ff53af0db39604dbe2e7b9
mitsu25 <m.nakano65@gmail.com>
2017-07-12 20:32:04 +0900

    refactoring : move action "buy list" to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user!, only: :sell_list
+  before_action :authenticate_user!,     only: :sell_list
+  before_action :set_user_selling_items, only: [:show, :sell_list]
 
   def show
-    get_user_items(user_params[:id])
-    @follower = Follower.where(user_id: current_user.id).find_by(followed_user_id: user_params[:id]) if user_signed_in?
+    set_follower
   end
 
   def sell_list
-    get_user_items(current_user.id)
-    @items = @user.items.each_slice(3).to_a
+  end
+
+  def buy_list
+    set_user_bought_items
   end
 
 private
@@ -17,8 +19,21 @@ private
     params.permit(:id)
   end
 
-  def get_user_items(id)
-    @user     = User.find(id)
+  def set_user_selling_items
+    @user     = User.find(current_user.id)
     @items    = @user.items.each_slice(3).to_a
+  end
+
+  def set_user_bought_items
+    buyers = Buyer.where(user_id:current_user.id)
+    item_list = []
+    buyers.each do |buyer|
+      item_list << buyer.item
+    end
+    @items = item_list.each_slice(3).to_a
+  end
+
+  def set_follower
+    @follower = Follower.where(user_id: current_user.id).find_by(followed_user_id: user_params[:id]) if user_signed_in?
   end
 end
