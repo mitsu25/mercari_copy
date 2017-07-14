838f8693be9552c9c816d47a273de2d659ab5bad
mitsu25 <m.nakano65@gmail.com>
2017-07-14 22:49:10 +0900

    generate controller and set actions for my_page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,11 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!,     only: :sell_list
-  before_action :set_user_selling_items, only: [:show, :sell_list]
 
   def show
-    set_follower
-  end
-
-  def sell_list
-  end
-
-  def buy_list
-    set_user_bought_items
+    @user     = set_user
+    @follower = set_follower
+    @items    = @user.items.each_slice(3).to_a
   end
 
 private
@@ -19,21 +13,11 @@ private
     params.permit(:id)
   end
 
-  def set_user_selling_items
-    @user     = User.find(current_user.id)
-    @items    = @user.items.each_slice(3).to_a
-  end
-
-  def set_user_bought_items
-    buyers = Buyer.where(user_id:current_user.id)
-    item_list = []
-    buyers.each do |buyer|
-      item_list << buyer.item
-    end
-    @items = item_list.each_slice(3).to_a
+  def set_user
+    User.find(user_params[:id])
   end
 
   def set_follower
-    @follower = Follower.where(user_id: current_user.id).find_by(followed_user_id: user_params[:id]) if user_signed_in?
+    Follower.where(user_id: current_user.id).find_by(followed_user_id: user_params[:id]) if user_signed_in?
   end
 end
