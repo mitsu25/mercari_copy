2b7b344f7eea35ca213812c0089d5c1a60073b61
mitsu25 <m.nakano65@gmail.com>
2017-07-15 20:46:24 +0900

    set view and controller to show likes status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def has_user_liked?(item)
+    Like.where(item_id:item.id,  user_id:current_user.id).present?
+  end
+
+  def does_item_have_likes?(item)
+    item.likes.present?
+  end
+
+  def get_likes_number(item)
+    likes_number = item.likes.count
+    if likes_number > 0
+      return likes_number
+    else
+      return "&nbsp;".html_safe
+    end
+  end
 end
