f6644db5b4cc59367758f8cd1e107cbfa3574232
mitsu25 <m.nakano65@gmail.com>
2017-07-15 20:42:40 +0900

    set associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+#---------- Associations -----------------
   has_many :items
   has_many :buyers
   has_many :followers
+  has_many :likes
 end
