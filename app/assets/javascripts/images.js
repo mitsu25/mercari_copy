18b9395c651be634cc51199b0c4c560ab328ddd3
mitsu25 <m.nakano65@gmail.com>
2017-07-27 12:25:00 +0900

    fix_and_refactoring_images_change_function_in_item_show_page

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,15 +1,15 @@
 $(function(){
-  $(".item__image_box__upper img").bind("load",function(){
-    var ImgHeight = $(this).height();
-    $('.item__image_box__upper').css('height',ImgHeight);
-  });
 
-  $('.item_images img').click(function(){
+  var $largeImage   = $('.item__image_box__upper img')
+  var $thumbnail    = $('.item_thumbnails')
+  //クリックしたサムネイルを選択状態（白化）にし、
+  //サムネイルの画像を上部のイメージボックスに
+  $thumbnail.click(function(){
     var selectedClass = 'item__image_box__bottom__image--over';
     if($(this).hasClass(selectedClass) == false){
-      $('.item_images img').removeClass(selectedClass);
+      $thumbnail.removeClass(selectedClass);
       $(this).addClass(selectedClass);
-      $('.item__image_box__upper img').attr('src',$(this).attr('src'));
+      $largeImage.attr('src',$(this).attr('src'));
     };
     return false;
   }).filter(':eq(0)').click();
